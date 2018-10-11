61d60dac84e908ba6577e84d5a06678a05290dc8
Hunter <hjones2199@gmail.com>
2018-10-11 15:54:32 -0500

    Implemented clientside server sanity checks and graceful shutdowns.

--- client/palinchecker_client.py
+++ client/palinchecker_client.py
@@ -7,12 +7,24 @@ def query_server(msg, ip, port, buffSize):
     sock.send(msg.encode())
     reply = sock.recv(buffSize)
     sock.close()
-    return reply.decode()
+    return str(reply, 'ascii').rstrip().lstrip()
 
 def term_interface():
     buffSize = 2048 #not configurable in current server implementation without editing code
     IP = input("Enter IP address of palindrome server: ")
     PORT = int(input("Enter port number of palindrome server: "))
+
+    try: #Checks if server is running, and sanity checks output
+        print("Checking for active server on " + IP + ":" + str(PORT) + " ...")
+        if query_server("hannah", IP, PORT, buffSize) == "True":
+            print("Success")
+        else:
+            print("Failure: Protocol mismatch")
+            exit(1)
+    except Exception:
+        print("Failure: Cannot find route to host")
+        exit(1)
+    
     userIn = ""
     while(True):
         userIn = input("Enter a string to check for palindrome or q to quit: ")
