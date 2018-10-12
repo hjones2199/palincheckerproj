db2023f6e3871b1a1cdbea9f93bcb8e8aebf4395
Hunter <hjones2199@gmail.com>
2018-10-11 19:11:31 -0500

    Added docstrings to client

--- client/palinchecker_client.py
+++ client/palinchecker_client.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
+"""
+Command line client that interfaces over TCP with a server
+to determine whether a user specified string is a palindrome.
+"""
 import socket
 
 def query_server(msg, ip, port, buffSize):
+    """
+    Sends a string to the server over TCP and returns True
+    or False as a string depending on whether the server
+    detected that the string is a palindrome.
+    """
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((ip, port))
     sock.send(msg.encode())
@@ -10,6 +19,13 @@ def query_server(msg, ip, port, buffSize):
     return str(reply, 'ascii').rstrip().lstrip()
 
 def term_interface():
+    """
+    CLI interface that allows the user to connect to a palindrome
+    checking server, and verifies that the server can be reached.
+    After verification, a user input loop begins that checks with
+    the server to determine if the user's specified strings are
+    palindromes.
+    """
     buffSize = 2048 #not configurable in current server implementation without editing code
     IP = input("Enter IP address of palindrome server: ")
     PORT = int(input("Enter port number of palindrome server: "))
@@ -34,6 +50,7 @@ def term_interface():
             print("Server says: " + query_server(userIn, IP, PORT, buffSize))
 
 def main():
+    """Programs entry point, all it does is call term_interface()"""
     term_interface()
 
 if __name__ == '__main__':
